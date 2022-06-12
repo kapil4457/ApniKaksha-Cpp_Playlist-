10dcca4d3086df1239a58341d5cd9c796d229df5
Kapil Soni <kapilsoni54768161@gmail.com>
2022-06-12 18:06:36 +0530

    Vectors

---
+++ .history/vectors_20220612180612.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+
+    // Size
+    cout << "size : " << v.size() << endl;
+
+    // Iterator
+    cout << "via Iterator : " << endl;
+    vector<int>::iterator it;
+    for (it = v.begin(); it != v.end(); it++)
+    {
+        cout << *it << endl;
+    }
+
+    // Automatic detection
+    cout << "Via Auto : " << endl;
+    for (auto element : v)
+    {
+        cout << element << endl;
+    }
+
+    // Pop function
+    v.pop_back();
+
+    v.sort(v.begin(), v.end());
+
+    return 0;
+}
