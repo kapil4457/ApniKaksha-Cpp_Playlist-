689bcef19ec8b89fb158011c7133c183df4a30ad
Kapil Soni <kapilsoni54768161@gmail.com>
2022-06-22 09:46:35 +0530

    Build tree from inOrder and preOrder

--- binary_tree.cpp
+++ binary_tree.cpp
@@ -50,19 +50,62 @@ void postOrder(Node *root)
     postOrder(root->right);
     cout << root->data << " ";
 }
+
+// Building tree form Inorder  and PostOrder travesal array
+
+int search(int inOrder[], int start, int end, int curr)
+{
+    for (int i = start; i <= end; i++)
+    {
+        if (inOrder[i] == curr)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+Node *buildTree(int preOrder[], int inOrder[], int start, int end)
+{
+    static int idx = 0;
+
+    if (start > end)
+    {
+        return NULL;
+    }
+    int curr = preOrder[idx];
+    idx++;
+    Node *root = new Node(curr);
+    if (start == end)
+    {
+        return root;
+    }
+    int pos = search(inOrder, start, end, curr);
+    root->left = buildTree(preOrder, inOrder, start, pos - 1);
+    root->right = buildTree(preOrder, inOrder, pos + 1, end);
+    return root;
+}
+
 int main()
 {
-
-    struct Node *root = new Node(1);
-    root->left = new Node(2);
-    root->right = new Node(3);
-    root->left->left = new Node(4);
-    root->left->right = new Node(5);
-    root->right->left = new Node(6);
-    root->right->right = new Node(7);
+    /*---------Tree Traversal-------*/
+    // struct Node *root = new Node(1);
+    // root->left = new Node(2);
+    // root->right = new Node(3);
+    // root->left->left = new Node(4);
+    // root->left->right = new Node(5);
+    // root->right->left = new Node(6);
+    // root->right->right = new Node(7);
     // preOrder(root);
     // inOrder(root);
-    postOrder(root);
+    // postOrder(root);
+
+    /*---------Tree Creation-------*/
+
+    int preorder[] = {1, 2, 4, 3, 5};
+    int inorder[] = {4, 2, 1, 5, 3};
+    Node *root = buildTree(preorder, inorder, 0, 4);
+    inOrder(root);
 
     return 0;
 }
