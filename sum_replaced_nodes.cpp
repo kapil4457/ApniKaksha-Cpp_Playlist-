2572bc5c8a2f2486abacaff847c2c0ebafe3ee92
Kapil Soni <kapilsoni54768161@gmail.com>
2022-06-25 20:34:01 +0530

    sum Replacement

---
+++ sum_replaced_nodes.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+public:
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int replacement(Node *root)
+{
+
+    if (root == NULL)
+    {
+        return 0;
+    }
+
+    root->data += replacement(root->left) + replacement(root->right);
+
+    return root->data;
+}
+int main()
+{
+
+    struct Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+
+    replacement(root);
+    cout << root->data;
+    return 0;
+}
